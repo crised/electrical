c96834acaa54d479ec55653d95373d4971c5680b
csjpeter <csjpeter@gmail.com>
2013-09-10 16:26:09 +0200

    reading all required values

--- arm/serial/main.c
+++ arm/serial/main.c
@@ -1,20 +1,17 @@
+/*
+ * Author: Peter Csaszar - 10 Sept 2013
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include <modbus-rtu.h>
 
 /* 10.679­84: Voltages of Line 1, 2, 3. 15’ average.(this should be Phase to Neutral measures)
-
-10.685­90: Voltages between line 1­3, 2­3, 3­1.15’ average.
-10.691­96: Current in lines 1,2,3. 15’ average.
-10.697­02: Active power in lines 1,2,3. (Watt) 15’ average. (Divide by 1000, to get KW)
-10.705­12: Reactive power in lines 1,2,3. (VAR) 15’ average.(Divide by 1000, to get KVAR)
-10.721­22: Current in Neutral (L0). 15’ Average.
-10.723­24: Frequency. 15’ Average. Close to 50 Hz, almost constant.
-10.725­30: Apparent power in lines 1,2,3. (VA) 15’ average.(Divide by 1000, to get KVA)
 
 Energy (this is measured continously), so we should refresh it at different interval:
 
@@ -47,6 +44,139 @@ I think energy can be reseted so we have to deal with this exception somehow.
 #define VOLTAGE_23 4 // * 2 + REGISTERS_FROM = 10687
 #define VOLTAGE_31 5 // * 2 + REGISTERS_FROM = 10689
 
+/* 10691-10696 Current in lines 1, 2, 3; 15 min average */
+#define CURRENT_1 6 // * 2 + REGISTERS_FROM = 10691
+#define CURRENT_2 7 // * 2 + REGISTERS_FROM = 10693
+#define CURRENT_3 8 // * 2 + REGISTERS_FROM = 10695
+
+/* 10697-10702 Active Power in lines (Watt) 1, 2, 3; 15 min average */
+#define ACT_POWER_1 9 // * 2 + REGISTERS_FROM = 10697
+#define ACT_POWER_2 10 // * 2 + REGISTERS_FROM = 10699
+#define ACT_POWER_3 11 // * 2 + REGISTERS_FROM = 10701
+
+/* 10705-10710 Reactive Power in lines (VAR) 1, 2, 3; 15 min average */
+#define REACT_POWER_1 13 // * 2 + REGISTERS_FROM = 10705
+#define REACT_POWER_2 14 // * 2 + REGISTERS_FROM = 10707
+#define REACT_POWER_3 15 // * 2 + REGISTERS_FROM = 10709
+
+/* 10721-10722 Current in Neutral (L0); 15 min average */
+#define CURRENT_N 20 // * 2 + REGISTERS_FROM = 10721
+
+/* 10723-10724 Frequency; 15 min average */
+#define FREQUENCY 21 // * 2 + REGISTERS_FROM = 10723
+
+/* 10725-10730 Apparent power in lines (VA) 1, 2, 3; 15 min average */
+#define APP_POWER_1 23 // * 2 + REGISTERS_FROM = 10725
+#define APP_POWER_2 24 // * 2 + REGISTERS_FROM = 10727
+#define APP_POWER_3 25 // * 2 + REGISTERS_FROM = 10729
+
+/* 7980 Active Total Energy (Wh) */
+#define ACT_TOTAL_ENERGY 7980
+
+void readAndSendEnergy(modbus_t * ctx, long unsigned counter)
+{
+	uint16_t tab_reg = 0;
+	int rc = modbus_read_registers(ctx, ACT_TOTAL_ENERGY, 1, &tab_reg);
+	if (rc == -1) {
+		fprintf(stderr, "%s\n", modbus_strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	time_t t = time(NULL);
+	char json_data[JSON_BUFFER_SIZE] = {0};
+
+	int c = snprintf(json_data, JSON_BUFFER_SIZE, "{"
+			"\"TimeStamp\":\"%s\","
+			"\"id\":%lu,"
+			"\"energy\":%u"
+			"}",
+			asctime(gmtime(&t)),
+			counter,
+			tab_reg
+			);
+	if(c < 0) {
+		fprintf(stderr, "Failed to format json data using snprintf: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	if(JSON_BUFFER_SIZE <= c){
+		fprintf(stderr, "Buffer is not big enough (%u) to format json data. %s\n", JSON_BUFFER_SIZE, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	puts(json_data);
+}
+
+void readAndSendDatas(modbus_t * ctx, long unsigned counter)
+{
+	uint16_t tab_reg[NUM_OF_REGISTERS] = {0};
+	int rc = modbus_read_registers(ctx, REGISTERS_FROM, NUM_OF_REGISTERS, tab_reg);
+	if (rc == -1) {
+		fprintf(stderr, "%s\n", modbus_strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	time_t t = time(NULL);
+	char json_data[JSON_BUFFER_SIZE] = {0};
+
+	int c = snprintf(json_data, JSON_BUFFER_SIZE, "{"
+			"\"TimeStamp\":\"%s\","
+			"\"id\":%lu,"
+			"\"voltage1\":%u,"
+			"\"voltage2\":%u,"
+			"\"voltage3\":%u,"
+			"\"voltage13\":%u,"
+			"\"voltage23\":%u,"
+			"\"voltage31\":%u,"
+			"\"current1\":%u,"
+			"\"current2\":%u,"
+			"\"current3\":%u,"
+			"\"actPower1\":%u,"
+			"\"actPower2\":%u,"
+			"\"actPower3\":%u,"
+			"\"reactPower1\":%u,"
+			"\"reactPower2\":%u,"
+			"\"reactPower3\":%u,"
+			"\"currentN\":%u,"
+			"\"frequency\":%u,"
+			"\"appCurrent1\":%u,"
+			"\"appCurrent2\":%u,"
+			"\"appCurrent3\":%u"
+			"}",
+			asctime(gmtime(&t)),
+			counter,
+			tab_reg[VOLTAGE_1],
+			tab_reg[VOLTAGE_2],
+			tab_reg[VOLTAGE_3],
+			tab_reg[VOLTAGE_12],
+			tab_reg[VOLTAGE_23],
+			tab_reg[VOLTAGE_31],
+			tab_reg[CURRENT_1],
+			tab_reg[CURRENT_2],
+			tab_reg[CURRENT_3],
+			tab_reg[ACT_POWER_1],
+			tab_reg[ACT_POWER_2],
+			tab_reg[ACT_POWER_3],
+			tab_reg[REACT_POWER_1],
+			tab_reg[REACT_POWER_2],
+			tab_reg[REACT_POWER_3],
+			tab_reg[CURRENT_N],
+			tab_reg[FREQUENCY],
+			tab_reg[APP_POWER_1],
+			tab_reg[APP_POWER_2],
+			tab_reg[APP_POWER_3]
+			);
+	if(c < 0) {
+		fprintf(stderr, "Failed to format json data using snprintf: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	if(JSON_BUFFER_SIZE <= c){
+		fprintf(stderr, "Buffer is not big enough (%u) to format json data. %s\n", JSON_BUFFER_SIZE, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	puts(json_data);
+}
+
 int main(int argc, char *args[])
 {
 	const char * serial_device = "/dev/ttyS0";
@@ -75,40 +205,15 @@ int main(int argc, char *args[])
 		exit(EXIT_FAILURE);
 	}
 
-	uint16_t tab_reg[NUM_OF_REGISTERS] = {0};
-	int rc = modbus_read_registers(ctx, REGISTERS_FROM, NUM_OF_REGISTERS, tab_reg);
-	if (rc == -1) {
-		fprintf(stderr, "%s\n", modbus_strerror(errno));
-		exit(EXIT_FAILURE);
-	}
+	long unsigned counter = 0;
 
-	char json_data[JSON_BUFFER_SIZE] = {0};
+	while(1){
+		if(counter % 4)
+			readAndSendDatas(ctx, counter++);
+		readAndSendEnergy(ctx, counter++);
 
-	int c = snprintf(json_data, JSON_BUFFER_SIZE, "{"
-			"\"voltage1\":%u,"
-			"\"voltage2\":%u,"
-			"\"voltage3\":%u,"
-			"\"voltage13\":%u,"
-			"\"voltage23\":%u,"
-			"\"voltage31\":%u"
-			"}",
-			tab_reg[VOLTAGE_1],
-			tab_reg[VOLTAGE_2],
-			tab_reg[VOLTAGE_3],
-			tab_reg[VOLTAGE_12],
-			tab_reg[VOLTAGE_23],
-			tab_reg[VOLTAGE_31]
-			);
-	if(c < 0) {
-		fprintf(stderr, "Failed to format json data using snprintf: %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
+		usleep(1 * 1000 * 1000); // 5 seconds
 	}
-	if(JSON_BUFFER_SIZE <= c){
-		fprintf(stderr, "Buffer is not big enough (%u) to format json data. %s\n", JSON_BUFFER_SIZE, strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-
-	puts(json_data);
 
 	modbus_close(ctx);
 	modbus_free(ctx);
